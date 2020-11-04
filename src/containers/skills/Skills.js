d9fe0c4ec10c432c98cd6a2814cbeb33d424c2dc
Sherif Abdel-Naby <sherifabdlnaby@gmail.com>
2020-11-04 17:07:37 +0200

    Add new SVGs

--- src/containers/skills/Skills.js
+++ src/containers/skills/Skills.js
@@ -14,7 +14,7 @@ export default function Skills() {
           <div className="skills-image-div">
             <img
               alt="Saad Working"
-              src={require("../../assets/images/developerActivity.svg")}
+              src={require("../../assets/images/undraw_hacker_mindset_gjwq.svg")}
             ></img>
           </div>
         </Fade>
